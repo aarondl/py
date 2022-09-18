9b6cca6a2773d39d2e203b0bb0641c6890282318
Aaron L <aaron@intensifying.dev>
2022-09-17 22:46:54 -0700

    Change pip/pip3 -> python/python3 -m pip

--- src/bin/pipy.rs
+++ src/bin/pipy.rs
@@ -1,16 +1,18 @@
 fn main() {
     let mut args = std::env::args();
-    args.next();
-
-    run(args.collect())
+    args.next(); // drop the `pipy` arg
+    let args = args.collect::<Vec<String>>();
+    let mut pip_args = vec!["-m".to_owned(), "pip".to_owned()];
+    pip_args.extend(args);
+    run(pip_args);
 }
 
 #[cfg(target_os = "windows")]
 fn run(args: Vec<String>) {
-    pylib::run_command_with_args("pip3.exe", args);
+    pylib::run_command_with_args("python.exe", args);
 }
 
 #[cfg(not(target_os = "windows"))]
 fn run(args: Vec<String>) {
-    pylib::run_command_with_args("pip3", args);
+    pylib::run_command_with_args("python3", args);
 }
